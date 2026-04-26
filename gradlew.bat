0c79622b4a6c85ec9b9cbd576cb2f78a467067fd
SkyL9ne <annapeltola7@gmail.com>
2026-04-26 10:39:27 +0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,14 +1,14 @@
-@if "%DEBUG%" == "" @echo off
-@rem ##########################################################################
-@rem
-@rem  Gradle startup script for Windows
-@rem
-@rem ##########################################################################
+@if "%DEBUG%" == "" @echo OFF
+@REM ##########################################################################
+@REM
+@REM  Gradle startup script for Windows
+@REM
+@REM ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
+@REM Set local scope for the variables with Windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+@REM Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
 set DIRNAME=%~dp0
@@ -16,73 +16,73 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+@REM Find java.exe
+if defined JAVA_HOME GOTO findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
+if "%ERRORLEVEL%" == "0" GOTO init
 
-echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+ECHO.
+ECHO ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+ECHO.
+ECHO Please set the JAVA_HOME variable in your environment to match the
+ECHO location of your Java installation.
 
-goto fail
+GOTO fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto init
+if exist "%JAVA_EXE%" GOTO init
 
-echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
-echo.
-echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Java installation.
+ECHO.
+ECHO ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+ECHO.
+ECHO Please set the JAVA_HOME variable in your environment to match the
+ECHO location of your Java installation.
 
-goto fail
+GOTO fail
 
 :init
-@rem Get command-line arguments, handling Windowz variants
+@REM Get command-line arguments, handling Windowz variants
 
-if not "%OS%" == "Windows_NT" goto win9xME_args
-if "%@eval[2+2]" == "4" goto 4NT_args
+if NOT "%OS%" == "Windows_NT" GOTO win9xME_args
+if "%@eval[2+2]" == "4" GOTO 4NT_args
 
 :win9xME_args
-@rem Slurp the command line arguments.
+@REM Slurp the command line arguments.
 set CMD_LINE_ARGS=
 set _SKIP=2
 
 :win9xME_args_slurp
-if "x%~1" == "x" goto execute
+if "x%~1" == "x" GOTO execute
 
 set CMD_LINE_ARGS=%*
-goto execute
+GOTO execute
 
 :4NT_args
-@rem Get arguments from the 4NT Shell from JP Software
+@REM Get arguments from the 4NT Shell from JP Software
 set CMD_LINE_ARGS=%$
 
 :execute
-@rem Setup the command line
+@REM Setup the command line
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-@rem Execute Gradle
+@REM Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
 :end
-@rem End local scope for the variables with windows NT shell
-if "%ERRORLEVEL%"=="0" goto mainEnd
+@REM End local scope for the variables with Windows NT shell
+if "%ERRORLEVEL%"=="0" GOTO mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
-rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 1
+REM Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+REM the _cmd.exe /c_ return code!
+if  NOT "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+exit /B 1
 
 :mainEnd
 if "%OS%"=="Windows_NT" endlocal
